e6f1d44b26b7211b68c1b571928db7ff9b29c10f
copticgem <g@g.com>
2016-07-02 14:58:32 -0700

    File loader

---
+++ src/ArabicInterpretation/ArabicInterpretation/Core/FileHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class FileHelper
+    {
+        private static string DefaultPrefix = "Core.Resources.";
+
+        public static async Task<string> GetFile(
+            Author author,
+            bool isNT,
+            int bookNumber,
+            int chapterNumber)
+        {
+            string path = string.Empty;
+
+            // Author
+            if (author == Author.FrAntonios)
+            {
+                path += "Father_Antonious_Fekry.";
+            }
+            else if (author == Author.FrTadros)
+            {
+                path += "Father_Tadros_Yacoub_Malaty.";
+            }
+            else
+            {
+                throw new InvalidOperationException("Unexpected author: " + author);
+            }
+
+            // NT and OT
+            if (isNT)
+            {
+                path += "nt.";
+            }
+            else
+            {
+                path += "ot.";
+            }
+
+            path = DefaultPrefix + path + "_" + bookNumber + "." + chapterNumber + ".txt";
+
+            var assembly = typeof(FileHelper).GetTypeInfo().Assembly;
+            using (Stream stream = assembly.GetManifestResourceStream(path))
+            {
+                if (stream == null)
+                {
+                    throw new InvalidOperationException("File not found: " + path);
+                }
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+        }
+    }
+}
